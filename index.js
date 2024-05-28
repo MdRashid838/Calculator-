adc1dff9d6cb5992d93afaeabe20ec72b95f4c46
MdRashid838 <160717010+MdRashid838@users.noreply.github.com>
2024-05-28 16:10:58 +0530

    update

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 let string = "";
+let input = document.querySelector("input");
 let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click', (e)=>{
         if(e.target.innerHTML == '='){
            string = eval(string)
-           document.querySelector('input').value = string;
+          input.value = string;
+        } else{
+            string = string + e.target.innerHTML;
+           input.value = string;
+            console.log(e.target)
         }
-        else if(e.target.innerHTML == 'C'){
+       if(e.target.innerHTML ==='AC'){
            string = "";
-           document.querySelector('input').value = string;
-        }
-        else{
-        console.log(e.target);
-        string = string + e.target.innerHTML;
-        document.querySelector('input').value = string;
+          input.value = "";
+           
+       }else if(e.target.innerHTML === 'DEL'){
+         string = string.slice(0, -4);
+       input.value = string;
         }
     })
 })
